d8df09cf71f562751bc67a949c606e3ef20bd43a
aknotj <100099666+aknotj@users.noreply.github.com>
2022-04-13 13:09:12 +0000

    [Start] Bookers2-remake

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+
+  devise_for :users
+  root to: "homes#top"
+  get 'home/about' => 'homes#about', as: "about"
+  resources :books, only: [:new, :create, :edit, :update, :show, :index, :delete]
+  resources :users, only: [:edit, :update, :show, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
